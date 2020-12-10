d0ac65167d4c1b6df721804c690d1c432e140ccf
Fleuron Gnadekpa <vilfleur@gmail.com>
2020-12-10 22:45:08 +0100

    Created Pandas Eating Lots with hard coded data

---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Layout from "../components/layout"
+
+export default function About() {
+  return (
+    <Layout>
+      <h1>About Pandas Eating Lots</h1>
+      <p>We are the only site running on your computer dedicated to showing the best photos and videos of pandas eating lots of foods.</p>
+    </Layout>
+  )
+}
