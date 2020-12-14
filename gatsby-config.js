1402c1d7ca2e05d16fe78bacf9b071fcc59b12a1
Fleuron Gnadekpa <vilfleur@gmail.com>
2020-12-14 09:57:03 +0100

    Completed Preparations for Live

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,23 @@
 module.exports = {
   siteMetadata: {
     title: `Pandas Eating Lots`,
+    description: `A simple description about Pandas Eating Lots`,
+    author: `fleuronvilik with gatsbyjs`,
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#6b37bf`,
+        theme_color: `#6b37bf`,
+        display: `standalone`,
+        icon: `src/images/icon.png`,
+      },
+    },
+    `gatsby-plugin-offline`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
